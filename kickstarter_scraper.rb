075bb29c190010636ec23935e5e191812bd5d1cb
Rupa <rupa211984@gmail.com>
2019-11-05 14:52:06 -0800

    Pass tests

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,16 @@
-# require libraries/modules here
+require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  projects = kickstarter.css(".project-card")
+
+  projects.each_with_object({}) do |project, hash|
+    hash[project.css('.bbcard_name a').text.strip] = {
+      image_link: project.css('img').attribute('src').value,
+      description: project.css('p.bbcard_blurb').text.strip,
+      location: project.css('.location-name').text.strip,
+      percent_funded: project.css('.funded strong').text.strip[0...-1].to_i
+    }
+  end
 end
